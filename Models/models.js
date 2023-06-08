613115f3dfb9a04032222537e5b5c9f4dd125d32
dev30k <66430325+dev30k@users.noreply.github.com>
2023-06-08 23:43:05 +0300

    changes to db name

--- Models/models.js
+++ Models/models.js
@@ -2,7 +2,7 @@ const db = require('../database/db');
 const createScrappedData = (tweetData) => {
     const {  name,username, post_date,tweet_text, reply_count, retweet_count, like_count} = tweetData;
 
-    const sql = `INSERT INTO tweets (name,username, post_date,tweet_text, reply_count, retweet_count, like_count) VALUES (?, ?, ?, ?, ?,?,?)`;
+    const sql = `INSERT INTO twitter_Data (name,username, post_date,tweet_text, reply_count, retweet_count, like_count) VALUES (?, ?, ?, ?, ?,?,?)`;
 
     return new Promise((resolve, reject) => {
         db.query(sql, [name,username, post_date,tweet_text, reply_count, retweet_count, like_count], (err, results) => {
